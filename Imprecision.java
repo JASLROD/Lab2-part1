bdb022327b5403c1ffdc458968f6b29d2e0b7113
Jasmin Leonrodriguez <jleonrodriquez@student.sdccd.edu>
2025-02-11 11:38:39 -0800

    Initial sharing of project

---
+++ Imprecision.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class Imprecision here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Imprecision
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Imprecision
+     */
+    public Imprecision()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
