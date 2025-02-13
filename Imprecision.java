3c72f482cdd9e060dd4ea8085ed3591ebc81de12
Jasmin Leonrodriguez <jleonrodriquez@student.sdccd.edu>
2025-02-13 12:01:26 -0800

    what i did thursday in class

--- Imprecision.java
+++ Imprecision.java
@@ -2,11 +2,21 @@
 /**
  * This is the second Lab Imprecision file .
  *
- * Jasmin Leonrodriguez, jleonrodriquez@student.sdccd.edu
+ * @author Jasmin Leonrodriguez, jleonrodriquez@student.sdccd.edu
  * @version v1.0
- * @since 2/11/2025
+ * @since 2/11/2025 //Replit (Jasmin Leonrodriguez, v1.0)
  */
 public class Imprecision
 {
-    public static
-}
+    public static void main (String[]args){ 
+        double x=1.23456789E204;, y,z;
+        double y, z;
+        y=1/x;
+        z=x*y;
+        
+        System.out.println(The value of x is: " + x);
+        //similar for both y and z
+        System.out.println(The value of 1 - z is: " + (1-z));
+    }// end Main()
+        
+}//end Main 
