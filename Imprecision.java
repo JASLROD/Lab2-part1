45e2c06f7578f158d534e8bd21f1ca9b3f09ad02
Jasmin Leonrodriguez <jleonrodriquez@student.sdccd.edu>
2025-02-15 20:07:36 -0800

    finished lab2pt1at home

--- Imprecision.java
+++ Imprecision.java
@@ -4,19 +4,21 @@
  *
  * @author Jasmin Leonrodriguez, jleonrodriquez@student.sdccd.edu
  * @version v1.0
- * @since 2/11/2025 //Replit (Jasmin Leonrodriguez, v1.0)
+ * @since 2/11/2025
  */
 public class Imprecision
 {
     public static void main (String[]args){ 
-        double x=1.23456789E204;, y,z;
-        double y, z;
+        double x = 1.23456789E204;
+        double y,z;
         y=1/x;
         z=x*y;
         
-        System.out.println(The value of x is: " + x);
+        System.out.println("The value of x is: " + x);
+        System.out.println("The value of y is: " + y);
+        System.out.println("The value of z is: " + z);
         //similar for both y and z
-        System.out.println(The value of 1 - z is: " + (1-z));
+        System.out.println("The value of 1 - z is: " + (1-z));
     }// end Main()
         
-}//end Main 
+}//end Imprecision 
