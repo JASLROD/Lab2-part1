07767031964b263e8aa8f1597160d9cf0ed9418e
Jasmin Leonrodriguez <jleonrodriquez@student.sdccd.edu>
2025-02-23 18:28:12 -0800

    Made corrections and these are the updated versions

--- Lab2PC3.java
+++ Lab2PC3.java
@@ -1,16 +1,15 @@
 
 /**
  *Lab2PC3 display information on seperate lines
- *
+ *the program will be displaying information on 3 seperate lines using a single Println
  * @author Jasmin Leonrodriguez, jleonrodriquez@student.sdccd.edu
- * @version v1.0
+ * @version v1.1
  * @since 2/15/2025
  */
 public class Lab2PC3
 {
    public static void main(String[] args){
-       System.out.println("Jasmin Leonrodriguez");
-       System.out.println("Mathematics");
-       System.out.println("I go to salsa classes for fun.");
+       System.out.println("Jasmin Leonrodriguez\nMathematics\nI go to salsa classes for fun.");
+    
    }
 }
