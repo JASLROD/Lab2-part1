e0eeee5a239b523eb566f3e0effed0ce29d1d697
Jasmin Leonrodriguez <jleonrodriquez@student.sdccd.edu>
2025-02-23 18:10:42 -0800

    uploading into one respo not different ones

---
+++ Lab2PC3.java
@@ -0,0 +1,16 @@
+
+/**
+ *Lab2PC3 display information on seperate lines
+ *
+ * @author Jasmin Leonrodriguez, jleonrodriquez@student.sdccd.edu
+ * @version v1.0
+ * @since 2/15/2025
+ */
+public class Lab2PC3
+{
+   public static void main(String[] args){
+       System.out.println("Jasmin Leonrodriguez");
+       System.out.println("Mathematics");
+       System.out.println("I go to salsa classes for fun.");
+   }
+}
---
+++ Lab2PC4.java
@@ -0,0 +1,20 @@
+
+/**
+ *Lab2PC4 Star Pattern
+ *
+ * @author Jasmin Leonrodriguez, jleonrodriquez@student.sdccd.edu
+ * @version v1.0
+ * @since 2/15/2025
+ */
+public class Lab2PC4
+{
+    public static void main(String[] args){
+       System.out.println("   *   ");
+       System.out.println("  ***  ");
+       System.out.println(" ***** ");
+       System.out.println("*******");
+       System.out.println(" ***** ");
+       System.out.println("  ***  ");
+       System.out.println("   *   ");
+    }
+}
